ccf5f217c3c137abbf7f813b1a0d42e6493e4e4e
Apoorva Saini <apoorvasaini2008@gmail.com>
2026-08-26 15:12:47 +0530

    Create PerfectNumber.java

---
+++ PerfectNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        if (num > 0) {
+            for (int i = 1; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == num) {
+                System.out.println(num + " is a Perfect Number.");
+            } else {
+                System.out.println(num + " is not a Perfect Number.");
+            }
+        } else {
+            System.out.println("Please enter a positive number.");
+        }
+
+        sc.close();
+    }
+}
