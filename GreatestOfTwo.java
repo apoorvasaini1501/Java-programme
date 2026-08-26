ddf74309f13556b4c05ca3da6df2f3e29dc15240
Apoorva Saini <apoorvasaini2008@gmail.com>
2026-08-26 14:53:30 +0530

    Create GreatestOfTwo.java

---
+++ GreatestOfTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GreatestOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(a + " is the greatest number.");
+        } else if (b > a) {
+            System.out.println(b + " is the greatest number.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+
+        sc.close();
+    }
+}
