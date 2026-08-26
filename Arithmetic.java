bf1bf8763cfbc7d33ffe8d9568c75158e893622b
Apoorva Saini <apoorvasaini2008@gmail.com>
2026-08-26 14:26:01 +0530

    Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -5,7 +5,7 @@ class Arithmetic {
     Scanner sc = new Scanner(System.in );
     System.out.println("enter a first number:");
     int a = sc.nextInt();
-    System out.println("enter a second number:");
+    System.out.println("enter a second number:");
     int b = sc.nextInt();
       System.out.println("Addition:"+ (a+b));
       System.out.println("Substraction:"+(a-b));
