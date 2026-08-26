e5765276c17312358770ea2b51e42b157cb843f1
apoorvasaini1501 <apoorvasaini2008@gmail.com>
2026-08-26 10:47:24 +0530

    Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+important java.util.scanner;
+class Arithmetic {
+  public static void main (string []args)
+  {
+    Scanner sc = new Scanner(System.in );
+    System.out.println("enter a first number:");
+    int a= 8
+    Sustem out.println("enter a second number:");
+    int b= 2
+      System.out.println("Addition:"+ (a+b));
+      System.out.println("Substraction:"+(a-b));
+      System.out.println("Multiplication:"+(a*b));
+      System.out.println("division:"+(a/b));
+      System.out.println("modulus:")+(a%b));
+  }
+}
