84115e91e4cc80118742ee9ded9e56e40d968e76
apoorvasaini1501 <apoorvasaini2008@gmail.com>
2026-08-26 10:49:28 +0530

    Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+class student {
+  public static void main (string [] args)
+  {
+  string name ="Apoorva";
+  int age ="18";
+  string college="COER University";
+
+  system.out.println("Name:"+name);
+  system.out.println("age:"+age);
+  system.out.println("college:"+college);
+  }
+  }
