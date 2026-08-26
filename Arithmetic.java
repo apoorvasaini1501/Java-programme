ca2e0a430449d68538b4bd69ad4c0cd341c03a11
Apoorva Saini <apoorvasaini2008@gmail.com>
2026-08-26 14:17:59 +0530

    Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,16 +1,16 @@
-important java.util.scanner;
+import java.util.scanner;
 class Arithmetic {
-  public static void main (string []args)
+  public static void main (String []args)
   {
     Scanner sc = new Scanner(System.in );
     System.out.println("enter a first number:");
-    int a= 8
-    Sustem out.println("enter a second number:");
-    int b= 2
+    int a = sc.nextInt();
+    System out.println("enter a second number:");
+    int b = sc.nextInt();
       System.out.println("Addition:"+ (a+b));
       System.out.println("Substraction:"+(a-b));
       System.out.println("Multiplication:"+(a*b));
-      System.out.println("division:"+(a/b));
-      System.out.println("modulus:")+(a%b));
+      System.out.println("Division:"+(a/b));
+      System.out.println("Modulus:")+(a%b));
   }
 }
